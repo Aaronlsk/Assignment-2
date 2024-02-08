fc4eb24fac80097812d4eddf6c3e3159cc9221e5
Aaron <S10258287@connect.np.edu.sg>
2024-02-08 19:47:31 +0800

    JavaScipt Additions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     let isLoggedIn = localStorage.getItem("isLoggedIn");
+    let accountLinkElement = document.getElementById("account-link");
 
     if (isLoggedIn) {
-        document.getElementById("account-link").innerHTML = '<a href="AccountDetails.html">Account</a>';
+        accountLinkElement.innerHTML = '<a href="AccountDetails.html">Account</a>';
+    }
+
+    let signOutButton = document.querySelector(".acc-buttons button:first-child");
+
+    if (signOutButton) {
+        signOutButton.addEventListener("click", function () {
+
+            localStorage.removeItem("isLoggedIn");
+            localStorage.removeItem("accountHolderName");
+
+            window.location.href = "Account.html";
+        });
     }
 });
